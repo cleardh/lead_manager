642c7b6acdc2a30fbedb7eb78ce9d2fc6660af8c
Dongha Kang <cleardh@hotmail.com>
2020-01-27 00:36:19 -0500

    django with react and redux

---
+++ leadmanager/frontend/src/reducers/leads.js
@@ -0,0 +1,28 @@
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from '../actions/types';
+
+const initialState = {
+  leads: []
+};
+
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case GET_LEADS:
+      return {
+        ...state,
+        leads: payload
+      };
+    case DELETE_LEAD:
+      return {
+        ...state,
+        leads: state.leads.filter(lead => lead.id !== payload)
+      };
+    case ADD_LEAD:
+      return {
+        ...state,
+        leads: [...state.leads, payload]
+      };
+    default:
+      return state;
+  }
+}
